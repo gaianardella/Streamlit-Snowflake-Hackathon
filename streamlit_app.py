ca9eb6d3bee3d4343a59bd652f9c35e8b0a62244
Gaia Nardella <105795404+gaianardella@users.noreply.github.com>
2023-04-29 00:46:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
-
 import streamlit as st
 import pandas as pd
 import snowflake.connector
 import pandas as pd
-from snowflake.snowpark.session import Session
+# from snowflake.snowpark.session import Session
