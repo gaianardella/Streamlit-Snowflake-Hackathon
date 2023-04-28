1ec85433de2336bcdddbdc27ef4a59a260317f7d
Gaia Nardella <105795404+gaianardella@users.noreply.github.com>
2023-04-29 01:07:20 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,8 +10,8 @@ if uploaded_file is not None:
   bytes_data_in_hex = uploaded_file.getvalue().hex()
 
   # Generate new image file name
-  file_name = 'img_' + str(uuid.uuid4())
+#   file_name = 'img_' + str(uuid.uuid4())
 
   # Write image data in Snowflake table
-  df = pd.DataFrame({"id": [file_name], "item": [bytes_data_in_hex]})
+  df = pd.DataFrame({"id": "1", "item": [bytes_data_in_hex]})
   session.write_pandas(df, "clothes_table")
