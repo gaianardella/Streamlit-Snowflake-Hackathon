eb772747317a67576ef644e09f301f4ea3007599
Gaia Nardella <105795404+gaianardella@users.noreply.github.com>
2023-04-29 16:21:53 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -184,7 +184,8 @@ if 'login' in st.session_state:
             if temperature == 'Hot':
                 # Execute the SQL query to select a random record with type = 'Sweater'
                 df =session.sql("SELECT item FROM clothes_table WHERE type = 'Sweater'")
-                row=df.sample(n = 1).collect()[0].ITEM
+#                 row=df.sample(n = 1).collect()[0].ITEM
+                row=df.sample(n=1).to_pandas()
                 st.write(row)
 
                 st.stop()
