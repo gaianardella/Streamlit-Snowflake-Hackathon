cfe354d561506a2ef1adb20ddd0439243b25f355
Gaia Nardella <105795404+gaianardella@users.noreply.github.com>
2023-04-29 13:45:28 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,19 +4,18 @@ import streamlit as st
 import pandas as pd
 # import snowflake.connector
 
-uploaded_file = st.file_uploader("Choose an image file", accept_multiple_files=False, label_visibility='hidden')
-if uploaded_file is not None:
-  # Convert image base64 string into hex 
-  bytes_data_in_hex = uploaded_file.getvalue().hex()
-  cnx = snowflake.connector.connect(**st.secrets["snowflake"])
-  id=1
-  item_selected="sweater"
-  with cnx.cursor() as my_cur:
-    my_cur.execute("insert into clothes_table values ('" +id+ "', '" +bytes_data_in_hex+ "', '" +item_selected+ "')")
+st.write("ciao")
 
-  # Generate new image file name
-#   file_name = 'img_' + str(uuid.uuid4())
-
-  # Write image data in Snowflake table
-#   df = pd.DataFrame({"id": "1", "item": [bytes_data_in_hex]})
-#   session.write_pandas(df, "clothes_table")
+# Create Session object
+# def create_session_object():
+#    connection_parameters = {
+#       "account": "<account_identifier>",
+#       "user": "<username>",
+#       "password": "<password>",
+#       "role": "<role_name>",
+#       "warehouse": "<warehouse_name>",
+#       "database": "ENVIRONMENT_DATA_ATLAS",
+#       "schema": "ENVIRONMENT"
+#    }
+#    session = Session.builder.configs(connection_parameters).create()
+#    return session
