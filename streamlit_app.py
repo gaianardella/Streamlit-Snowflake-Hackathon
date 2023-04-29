481f2416a7cc1e3f354b68eed58852c84c7d1715
Gaia Nardella <105795404+gaianardella@users.noreply.github.com>
2023-04-29 13:59:48 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,10 +10,22 @@ st.write("ciao")
 
 session = Session.builder.configs(st.secrets["snowflake"]).create()
 # Create Snowpark DataFrames that loads data
-snow_df_co2 = session.table("clothes_table")
+# snow_df_co2 = session.table("clothes_table")
 
 # Convert Snowpark DataFrames to Pandas DataFrames for Streamlit
-pd_df_co2 = snow_df_co2.to_pandas()
+# pd_df_co2 = snow_df_co2.to_pandas()
+
+uploaded_file = st.file_uploader("Choose an image file", accept_multiple_files=False, label_visibility='hidden')
+if uploaded_file is not None:
+  # Convert image base64 string into hex 
+  bytes_data_in_hex = uploaded_file.getvalue().hex()
+
+  # Generate new image file name
+  file_name = 'img_' + str(uuid.uuid4())
+
+  # Write image data in Snowflake table
+  df = pd.DataFrame({"FILE_NAME": [file_name], "IMAGE_BYTES": [bytes_data_in_hex]})
+  session.write_pandas(df, "IMAGES")
 
 
 
