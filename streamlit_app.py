2f03d776236a745a722fae4666a25c2565c501fa
Gaia Nardella <105795404+gaianardella@users.noreply.github.com>
2023-04-29 14:01:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -20,12 +20,12 @@ if uploaded_file is not None:
   # Convert image base64 string into hex 
   bytes_data_in_hex = uploaded_file.getvalue().hex()
 
-  # Generate new image file name
-  file_name = 'img_' + str(uuid.uuid4())
+#   # Generate new image file name
+#   file_name = 'img_' + str(uuid.uuid4())
 
-  # Write image data in Snowflake table
-  df = pd.DataFrame({"FILE_NAME": [file_name], "IMAGE_BYTES": [bytes_data_in_hex]})
-  session.write_pandas(df, "IMAGES")
+#   # Write image data in Snowflake table
+#   df = pd.DataFrame({"FILE_NAME": [file_name], "IMAGE_BYTES": [bytes_data_in_hex]})
+#   session.write_pandas(df, "IMAGES")
 
 
 
