93c693927fd7cbb2fed69c68e801432a4208ed04
Gaia Nardella <105795404+gaianardella@users.noreply.github.com>
2023-05-02 01:40:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -229,35 +229,35 @@ def generate_bottom(cnx, bottom_type):
 def buttons():
     for i in range(15):
             st.write("")
-        placeholder_like = st.empty()
-        placeholder_dislike = st.empty()
-        if 'preference' not in st.session_state:
-            st.session_state['preference'] = 0
+    placeholder_like = st.empty()
+    placeholder_dislike = st.empty()
+    if 'preference' not in st.session_state:
+        st.session_state['preference'] = 0
 
-        if 'button' not in st.session_state:
-            col4, col5 = st.columns(2)
+    if 'button' not in st.session_state:
+        col4, col5 = st.columns(2)
 
-            with col4:
-                for i in range(60):
-                    st.write("")
+        with col4:
+            for i in range(60):
+                st.write("")
 
-                placeholder_like = st.empty()
-                with placeholder_like:
-                    like = st.button("Like :thumbsup:", use_container_width=True)
-                    if like:
-                        st.session_state['button'] = True
-                        st.session_state['preference'] = 1
+            placeholder_like = st.empty()
+            with placeholder_like:
+                like = st.button("Like :thumbsup:", use_container_width=True)
+                if like:
+                    st.session_state['button'] = True
+                    st.session_state['preference'] = 1
 
-            with col5:
-                for j in range(16):
-                    st.write("")
+        with col5:
+            for j in range(16):
+                st.write("")
 
-                placeholder_dislike = st.empty()
-                with placeholder_dislike:
-                    dislike = st.button("Dislike :thumbsdown:", use_container_width=True)
-                    if dislike:
-                        st.session_state['button'] = True
-                        st.session_state['preference'] = -1
+            placeholder_dislike = st.empty()
+            with placeholder_dislike:
+                dislike = st.button("Dislike :thumbsdown:", use_container_width=True)
+                if dislike:
+                    st.session_state['button'] = True
+                    st.session_state['preference'] = -1
 
 def generate_outfit(temperature, flag_top, flag_bottom):
     if temperature == 'Hot':
