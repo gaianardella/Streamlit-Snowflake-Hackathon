400060f42af3357247a99e298278eb27ea4a19ee
Gaia Nardella <105795404+gaianardella@users.noreply.github.com>
2023-05-06 01:39:01 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -165,8 +165,9 @@ def check_colors(colors_top,colors_bottom):
             st.write(new_input)
             url = 'https://github.com/gaianardella/Streamlit-Snowflake-Hackathon/blob/main/my_model.pkl?raw=true'
             model_file = BytesIO(requests.get(url).content)
-            model = pickle.load(model_file)
+            st.write(model_file)
             st.stop()
+            model = pickle.load(model_file)
             prediction = model.predict(new_input)
             
             if prediction == 'yes':
