0d873cc6c557cac3dd90cca51b84208be7edff63
Gaia Nardella <105795404+gaianardella@users.noreply.github.com>
2023-05-05 11:33:17 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -205,12 +205,13 @@ def generate_top_bottom(top_type,bottom_type):
                     color_2=bottom.lower()
                     new_input = pd.DataFrame({'color_1': [color_1], 'color_2': [color_2]})
                     new_input = new_input.replace(color_dict)
+                    st.write(new_input)
                     url = 'https://github.com/gaianardella/Streamlit-Snowflake-Hackathon/blob/main/my_model.pkl?raw=true'
         #             model = pickle.load(BytesIO(requests.get(url).content))
                     model_file = BytesIO(requests.get(url).content)
                     model = pickle.load(model_file)
                     prediction = model.predict(new_input)
-                    st.write(prediction)
+#                     st.write(prediction)
 
 
             hex_str = random_row[0].strip('"')
