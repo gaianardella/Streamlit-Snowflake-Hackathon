8353ff526524d802e3c74221cf2e1f322e8ce40a
Gaia Nardella <105795404+gaianardella@users.noreply.github.com>
2023-05-05 11:54:33 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -206,10 +206,8 @@ def generate_top_bottom(top_type,bottom_type):
                     color_2=bottom.lower()
 #                     new_input = {'color_1': [color_1], 'color_2': [color_2]}
                     new_input = pd.DataFrame({'color_1': [color_1], 'color_2': [color_2]})
-#                     new_input = new_input.replace(color_dict)
-                    if new_input.empty:
-                        st.write('DataFrame is empty!')
-#                     st.write(new_input)
+                    new_input = new_input.replace(color_dict)
+                    st.write(new_input)
                     url = 'https://github.com/gaianardella/Streamlit-Snowflake-Hackathon/blob/main/my_model.pkl?raw=true'
         #             model = pickle.load(BytesIO(requests.get(url).content))
                     model_file = BytesIO(requests.get(url).content)
