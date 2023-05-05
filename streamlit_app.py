3d4fe624cbef7217628c9b93c3b7b3a75ef5405c
Gaia Nardella <105795404+gaianardella@users.noreply.github.com>
2023-05-05 10:37:36 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -168,7 +168,7 @@ def generate_top_bottom(top_type,bottom_type):
         for item in items_strings:
             my_cur.execute(f"SELECT item,colors FROM clothes_table sample row (1 rows) WHERE type = '{item}'")
             random_row = my_cur.fetchone()
-            colors = random_row[1].strip("[").strip("]")
+            colors = random_row[1].strip("[").strip("]").split(",")
             st.write(colors)
              #SELEZIONARE COLORI
            #funzione contorllo colori e restituisce true e continua oppure false e richiama generate_top_bottom
