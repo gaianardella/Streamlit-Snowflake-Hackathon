df5b7b8a31d7fc0ca47c3b13f9c3182415519a49
Gaia Nardella <105795404+gaianardella@users.noreply.github.com>
2023-05-05 10:56:50 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -168,10 +168,10 @@ def generate_top_bottom(top_type,bottom_type):
         for item in items_strings:
             my_cur.execute(f"SELECT item,colors FROM clothes_table sample row (1 rows) WHERE type = '{item}'")
             random_row = my_cur.fetchone()
-            colors = random_row[1].strip("[").strip("]").replace('"','').replace(' ','').split(",")
+            colors = random_row[1].strip("[").strip("]").replace('"','').split(",")
             for color in colors:
-                strip_color=color.strip('"')
-                st.write("This is the color:" +color)
+                strip_color = color.strip(" ")
+                st.write("This is the color:" +strip_color)
                 if item == "Sweater" or item=="T-Shirt":
                     top_colors.append(strip_color)
                 elif item == "Trousers" or item=="Shorts":
