681cc6e7e46b22f693046094e5548112c3e49a01
Gaia Nardella <105795404+gaianardella@users.noreply.github.com>
2023-05-05 12:13:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -207,7 +207,7 @@ def generate_top_bottom(top_type,bottom_type):
                 for bottom in bottom_colors:
                     color_1=top[1::].lower()
                     color_2=bottom.lower()
-                    st.write(f"color is:"+str(color_1)
+                    st.write("color is:"+str(color_1))
 #                     st.write(color_dict[color_1.strip(' ')])
 #                     new_input = {'color_1': [color_1], 'color_2': [color_2]}
 #                     new_input = pd.DataFrame({'color_1': [color_1], 'color_2': [color_2]})
